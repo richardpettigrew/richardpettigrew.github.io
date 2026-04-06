2118eeba7bb704938c958608e8f14760de42a12d
Wiglet1981 <Richard.Pettigrew@gmail.com>
2026-04-06 14:58:57 -0400

    Add files via upload

---
+++ sidebar.js
@@ -0,0 +1,15 @@
+document.getElementById('master-sidebar').innerHTML = `
+    <img src="your-photo.png" alt="Richard Pettigrew" class="profile-pic">
+    <h1>Richard Pettigrew</h1>
+    <p>Department of Philosophy, University of Bristol.</p>
+    <p>Richard.Pettigrew@bristol.ac.uk</p>
+    <p><a href="https://richardpettigrew.substack.com/">Substack</a></p>
+    
+    <nav>
+        <ul>
+            <li><a href="index.html">Home / Publications</a></li>
+            <li><a href="cv.html">CV</a></li>
+            <li><a href="talks.html">Talks</a></li>
+            </ul>
+    </nav>
+`;
