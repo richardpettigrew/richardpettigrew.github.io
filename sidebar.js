352396a03226650016fac0e82b2b6587a41e0442
richardpettigrew <Richard.Pettigrew@gmail.com>
2026-04-08 07:17:50 -0400

    Add files via upload

--- sidebar.js
+++ sidebar.js
@@ -3,13 +3,15 @@ document.getElementById('master-sidebar').innerHTML = `
     <h1>Richard Pettigrew</h1>
     <p>Department of Philosophy, University of Bristol.</p>
     <p>Richard.Pettigrew@bristol.ac.uk</p>
-    <p><a href="https://richardpettigrew.substack.com/">Substack</a></p>
+    <p><a href="https://richardpettigrew.substack.com/">Substack</a> | <a href="https://drive.google.com/file/d/1LrVwO-7uGRNT9vOJJv6JAZ4nlnGOTQN6/view?usp=sharing">CV</a></p>
     
     <nav>
         <ul>
-            <li><a href="index.html">Home / Publications</a></li>
-            <li><a href="cv.html">CV</a></li>
+            <li><a href="index.html">Writing</a></li>
             <li><a href="talks.html">Talks</a></li>
+            <li><a href="https://www.flickr.com/photos/204432557@N07/albums">Places I've been</a></li>
+            <li><a href="https://www.flickr.com/photos/204432557@N07/albums/72177720332985553/">Cats I've met</a></li>
+            <li><a href="https://www.flickr.com/photos/204432557@N07/albums/72177720332966067">Bluebell</a></li>
             </ul>
     </nav>
 `;
